a18107e27e808c8a4f2d0d4bfb64544976e7041e
szulcmaciej <szulcmaciej1@gmail.com>
2018-04-02 22:00:48 +0200

    backtracking working, tested on mini example

--- model.py
+++ model.py
@@ -3,12 +3,15 @@ class Variable:
         self.n = name
         self.d = domain
 
+    def __str__(self):
+        return str(self.n)
+
 
 class Constraint:
-    def __init__(self, con_fun, variable_set):
+    def __init__(self, con_fun, variables):
         self.f = con_fun
         # set
-        self.v = variable_set
+        self.v = variables
 
 
 class Problem:
@@ -21,11 +24,16 @@ class Problem:
         self.variables.append(Variable(name, domain))
 
     def add_constraint(self, constraint_fun, variables):
-        self.constraints.append(Constraint(constraint_fun, variables))
+        object_variables = []
+        for v in variables:
+            for obj_v in self.variables:
+                if obj_v.n == v:
+                    object_variables.append(obj_v)
+        self.constraints.append(Constraint(constraint_fun, object_variables))
 
     def check_constraints(self, constraints, solution_dict):
         results = []
-        solution_variables = solution_dict.keys
+        # solution_variables = solution_dict.keys
         for c in constraints:
             values = [solution_dict.get(variable) for variable in c.v]
             results.append(c.f(*values))
@@ -42,8 +50,14 @@ class Problem:
         return result
 
     def get_all_solutions(self, algorithm='backtracking'):
-        # TODO
         solutions = []
+
+        if algorithm == 'backtracking':
+            solution = {}
+            solutions = self._backtrack_all_solutions(solution)
+        elif algorithm == 'forward_checking':
+            # TODO
+            solutions = []
 
         return solutions
 
@@ -84,20 +98,40 @@ class Problem:
 
     def _get_constraints_with_variables(self, variables):
         verified_constraints = set()
-        vars_set = frozenset(variables)
+        # TODO change to frozenset
+        vars_set = variables
         for c in self.constraints:
-            if c.v in vars_set:
+            # if c.v is a subset of given variables
+            if set(c.v) <= vars_set:
                 verified_constraints.add(c)
         return verified_constraints
 
-    def _backtrack_all(self, solution, var_index, domain_index):
+    def _backtrack_all_solutions(self, current_solution):
+        valid_solutions = []
+        last_assigned_var_index = -1
+        current_variables = []
+        current_constraints = []
 
-        if domain_index + 1 < len(self.variables[var_index].d):
-            # TODO check constraints
-            current_variables = solution.keys
+        # if solution dict is not empty
+        if bool(current_solution):
+            last_assigned_var_index = len(current_solution) - 1
+            current_variables = set(current_solution)
             current_constraints = self._get_constraints_with_variables(current_variables)
 
-        else:
+        # check current solution, if valid add to valid_solutions[] or go deeper (to next variable)
 
+        # check if all constraints in current solution are met
+        if not current_solution or not current_constraints or min(self.check_constraints(current_constraints, current_solution)) is True:
+            # if all variables are assigned, save solution, otherwise go deeper
+            if set(current_variables) == set(self.variables):
+                valid_solutions.append(current_solution)
+            else:
+                # v - currently asigned variabled
+                v = self.variables[last_assigned_var_index + 1]
+                for value in v.d:
+                    solution = current_solution.copy()
+                    solution[v] = value
+                    valid_solutions = valid_solutions + self._backtrack_all_solutions(solution)
 
+        return valid_solutions
 
